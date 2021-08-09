507de52573626b84b4f87204632fa3d58494222b
Vineet Pandey <vineet1992.vp@gmail.com>
2021-08-09 16:11:12 +0530

    add new class

---
+++ src/main/java/com/in28minutes/microservices/currencyexchangeservice/rewrite/Kubernates.java
@@ -0,0 +1,9 @@
+package com.in28minutes.microservices.currencyexchangeservice.rewrite;
+
+public class Kubernates {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello");
+    }
+}
